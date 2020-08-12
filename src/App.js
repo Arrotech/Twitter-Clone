955e26f33662cb32f595ddb0146245e6ce930fef
Arrotech <arrotechdesign@gmail.com>
2020-08-12 20:34:16 +0300

    Build Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
 	return (
-    <div className="app">
-    <h1>Twitter Clone</h1>
+		<div className="app">
+			<Sidebar />
 		</div>
 	);
 }
