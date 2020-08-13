6c6a6e1c683b410d1bac4b6654ba9a81c4532391
Arrotech <arrotechdesign@gmail.com>
2020-08-13 16:48:21 +0300

    Design widget section

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import './Widgets.css';
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed } from 'react-twitter-embed';
 
 function Widgets() {
-	return <div className="widgets"></div>;
+	return (
+		<div className="widgets">
+			<div className="widgets__input">
+				<SearchIcon className="widgets__searchIcon" />
+				<input placeholder="Search Twitter" type="text" />
+			</div>
+			<div className="widgets__widgetContainer">
+			<h2>What's happening</h2>
+				<TwitterTweetEmbed tweetId={'858551177860055040'} />
+				<TwitterTimelineEmbed sourceType="profile" screenName="ArrotechDesign" options={{ height: 400 }} />
+
+				<TwitterShareButton
+					url={'https://facebook.com/cleverprogrammer'}
+					options={{ text: '#reactjs is awesome', via: 'cleverqazi' }}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default Widgets;
