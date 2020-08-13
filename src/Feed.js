f616bef0f1613cb6940d1af042b20aa5f95e61aa
Arrotech <arrotechdesign@gmail.com>
2020-08-13 20:47:48 +0300

    Read Data

--- src/Feed.js
+++ src/Feed.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Feed.css';
 import TweetBox from './TweetBox';
 import Post from './Post';
+import db from './firebase';
 
 function Feed() {
+	const [posts, setPosts] = useState([]);
+	useEffect(() => {
+		db.collection('posts').onSnapshot((snapshot) => setPosts(snapshot.docs.map((doc) => doc.data())));
+	}, []);
 	return (
 		<div className="feed">
-            <div className="feed__header"><h2>Home</h2></div>
-            <TweetBox/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
+			<div className="feed__header">
+				<h2>Home</h2>
+			</div>
+                  <TweetBox />
+                  {posts.map(post => (
+                        <Post 
+                        verified={post.verified}
+				displayName={post.displayName}
+				username={post.username}
+				text={post.text}
+				image={post.image}
+				avatar={post.avatar}/>
+                  ))}
 		</div>
 	);
 }
